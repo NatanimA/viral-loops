b6b39c54d80ac459be6b771574c693c4f5f16348
Natanim <natanimabesha@gmail.com>
2024-09-27 18:14:10 +0300

    Client host Bug Fixes

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 const HOST  = process.env.REACT_APP_HOST || 'http://localhost:8000';
-const CLIENT_HOST = process.env.REACT_APP_CLIENT_HOST || 'http://localhost:3000';
+const CLIENT_HOST = window.location.origin
 const LAYOUT_API = '/api/form'
 export const API = {
     HOST,
